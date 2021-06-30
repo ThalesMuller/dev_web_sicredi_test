0bbeaed06ca0228fed996434424843f95cdf3f3a
ThalesMuller <thales.em@gmail.com>
2021-06-30 06:26:41 -0300

    [ADD] Basic login screen

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from './styles';
+import { Input, Button } from '../../components';
+import { useAuth } from '../../hooks/auth';
+
+interface IState {
+	username: string | undefined;
+	password: string | undefined;
+}
 
 export default function Login(): JSX.Element {
+	const [data, setData] = useState<IState>(() => {
+		const username = 'admin';
+		const password = 'admin';
+		return { username, password };
+	});
+
+	const { signIn } = useAuth();
+
+	const handleLogin = () => {
+		const { username, password } = data;
+		if (username && password) {
+			try {
+				signIn({ username, password });
+			} catch (err) {
+				console.error(err);
+			}
+		}
+	};
+
 	return (
 		<Container>
-			Login
+			{/* <Input value={ } label={ } />
+			<Input value={ } label={ } /> */}
+			<Button onClick={handleLogin}>Login</Button>
 		</Container>
 	);
 }
