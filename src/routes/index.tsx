8b464d61dac43f798af008b2099c02d22436b0fa
ThalesMuller <thales.em@gmail.com>
2021-06-30 01:13:17 -0300

    [ADD] adding routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+
+import Route from './routes';
+import Home from '../pages/home';
+import Details from '../pages/details';
+import Login from '../pages/login';
+
+
+const Routes: React.FC = () => {
+	return (
+		<Switch>
+			<Route path='/' exact component={Login} />
+			<Route path='/home' exact component={Home} isPrivate />
+			<Route path='/new' component={Details} isPrivate />
+			<Route path='/details/:id' component={Details} isPrivate />
+			<Route path='/edit/:id' component={Details} isPrivate />
+		</Switch>
+	);
+};
+
+export default Routes;
