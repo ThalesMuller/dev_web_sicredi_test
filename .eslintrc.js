0258409c1de687becc8fcf08e7104a67ac54d88b
ThalesMuller <thales.em@gmail.com>
2021-06-30 01:10:57 -0300

    [DEL] removing unnecessary files and adding basic frameworks

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef */
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'warn',
+			'single'
+		],
+		'semi': [
+			'warn',
+			'always'
+		],
+		'react/prop-types': 'warn',
+	}
+};
