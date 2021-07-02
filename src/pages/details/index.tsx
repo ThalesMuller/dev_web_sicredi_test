372bba4360671817e241c2e85da1faa83d00b2eb
ThalesMuller <thales.em@gmail.com>
2021-07-02 02:11:18 -0300

    [ADD] Styling page for dragon details

--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Container from './styles';
+import Container, { Buttons, CustomImage } from './styles';
 import {
 	getDragon,
 	editDragon,
@@ -7,13 +7,22 @@ import {
 	deleteDragon,
 } from '../../services/api';
 import IDragon from '../../interfaces/IDragon';
-import { Button } from '../../components';
+import { Button, Input, DisplayId, CreatedAt } from '../../components';
 import { useHistory, useLocation } from 'react-router-dom';
+import { FiAlignCenter, FiCloudLightning, FiUser } from 'react-icons/fi';
+
+import dragon1 from '../../assets/dragon1.jpg';
+import dragon2 from '../../assets/dragon2.jpeg';
+import dragon3 from '../../assets/dragon3.jpg';
 
 enum Origin {
 	new,
 	edit,
 	view,
+}
+
+interface IDragonCard {
+	image?: string;
 }
 
 export default function Details(): JSX.Element {
@@ -27,8 +36,10 @@ export default function Details(): JSX.Element {
 		type: '',
 		name: '',
 	});
+	const [dragonCard, setDragonCard] = useState<IDragonCard>({});
 
 	useEffect(() => {
+		setDragonCard(getRandomDragon());
 		const path = locationParams.pathname.split('/')[1];
 		switch (path) {
 			case 'new':
@@ -59,13 +70,28 @@ export default function Details(): JSX.Element {
 			})();
 		}
 	}, [origin]);
+	const getRandomDragon = (): IDragonCard => {
+		const dragonsImages = [dragon1, dragon2, dragon3];
+
+		const dragon: IDragonCard = {
+			image: dragonsImages[
+				Math.floor(Math.random() * dragonsImages.length)
+			],
+		};
+
+		return dragon;
+	};
 
 	const handleDeleteDragon = async () => {
 		try {
 			const confirm = true;
 			if (confirm && dragon?.id) {
-				await deleteDragon(dragon.id);
-				history.push('/home');
+				try {
+					await deleteDragon(dragon.id);
+					history.push('/home');
+				} catch (error) {
+					console.error(error);
+				}
 			}
 		} catch (err) {
 			console.error(err);
@@ -80,59 +106,126 @@ export default function Details(): JSX.Element {
 
 	const submitDragon = async () => {
 		if (dragon) {
-			try {
-				let data;
-				if (origin === Origin.new) {
+			//try {
+			let data;
+			if (origin === Origin.new) {
+				try {
 					data = await newDragon(dragon);
-				} else if (origin === Origin.edit && dragon?.id) {
+				} catch (error) {
+					console.error(error);
+				}
+			} else if (origin === Origin.edit && dragon?.id) {
+				try {
 					data = await editDragon(dragon);
-				} else {
-					throw new Error(
-						`You can't edit or create in ${origin} mode`,
-					);
+				} catch (error) {
+					console.error(error);
 				}
-				if (data) {
-					setDragon(data?.data);
-					setOrigin(Origin.view);
-					if (origin === Origin.new) {
-						history.push(`/details/${data?.data?.id}`);
-					}
-				}
-			} catch (err) {
-				console.error(err);
+			} else {
+				throw new Error(`You can't edit or create in ${origin} mode`);
 			}
+			if (data) {
+				setDragon(data?.data);
+				setOrigin(Origin.view);
+				if (origin === Origin.new) {
+					history.push(`/details/${data?.data?.id}`);
+				}
+			}
+			/* } catch (err) {
+				console.error(err);
+			} */
+		}
+	};
+
+	const isPropValid = (prop: string): boolean => {
+		if (!prop) {
+			return false;
+		}
+
+		const dragonProps = ['id', 'name', 'type', 'createdAt', 'histories'];
+
+		const selectedProp = dragonProps.find((p) => p === prop);
+
+		if (selectedProp) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	const onChange = (value: string, field: string) => {
+		if (field && isPropValid(field)) {
+			setDragon({
+				...dragon,
+				[field]: value,
+			});
 		}
 	};
 
 	const renderDragon = () => {
 		return (
 			dragon && (
-				<div>
-					<div>
-						{dragon.id && <div>{dragon.id}</div>}
-						{dragon.name && <div>{dragon.name}</div>}
-						{dragon.type && <div>{dragon.type}</div>}
-						{dragon.createdAt && <div>{dragon.createdAt}</div>}
-						{dragon.histories && <div>{dragon.histories}</div>}
-					</div>
-
-					{origin === Origin.view && (
-						<Button onClick={() => handleEditDragon()}>
-							Editar Dragão
-						</Button>
-					)}
-					{origin !== Origin.new && (
-						<Button onClick={() => handleDeleteDragon()}>
-							Deletar Dragão
-						</Button>
-					)}
-					{origin !== Origin.view && (
-						<Button onClick={() => submitDragon()}>
-							Salvar Dragão
-						</Button>
-					)}
-					<br />
-				</div>
+				<>
+					<CustomImage image={dragonCard?.image} />
+					<form className="details-form">
+						{dragon.id && <DisplayId>{dragon.id}</DisplayId>}
+						{dragon.createdAt && (
+							<CreatedAt date={dragon.createdAt} />
+						)}
+						<Input
+							disabled={origin === Origin.view}
+							title="Nome"
+							defaultValue={dragon?.name}
+							onChange={onChange}
+							name="name"
+							icon={FiUser}
+							placeholder="Nome do Dragão"
+						/>
+						<Input
+							disabled={origin === Origin.view}
+							title="Tipo"
+							defaultValue={dragon?.type}
+							onChange={onChange}
+							name="type"
+							icon={FiCloudLightning}
+							placeholder="Tipo do Dragão"
+						/>
+						<Input
+							disabled={origin === Origin.view}
+							title="História"
+							defaultValue={dragon?.histories}
+							onChange={onChange}
+							name="histories"
+							icon={FiAlignCenter}
+							placeholder="História do Dragão"
+						/>
+						<Buttons>
+							{origin !== Origin.new && (
+								<Button
+									type="button"
+									onClick={() => handleDeleteDragon()}
+								>
+									Deletar
+								</Button>
+							)}
+							{origin === Origin.view && (
+								<Button
+									type="button"
+									onClick={() => handleEditDragon()}
+								>
+									Editar
+								</Button>
+							)}
+							{origin !== Origin.view && (
+								<Button
+									type="submit"
+									onClick={() => submitDragon()}
+								>
+									Salvar
+								</Button>
+							)}
+						</Buttons>
+					</form>
+				</>
 			)
 		);
 	};
--- src/pages/details/styles.ts
+++ src/pages/details/styles.ts
@@ -1,7 +1,63 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Container = styled.div`
-	background: white;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: calc(100vh - 47.61px);
+	width: 100%;
+	flex-direction: column;
+	max-width: 800px;
+	margin: 0 auto;
+	padding: 1px 15px 1px 15px;
+	
+	.details-form {
+		-webkit-box-shadow: 8px 8px 11px -4px rgba(0, 0, 0, 0.75);
+		-moz-box-shadow: 8px 8px 11px -4px rgba(0, 0, 0, 0.75);
+		box-shadow: 8px 8px 11px -4px rgba(0, 0, 0, 0.75);
+
+		padding: 1rem;
+		border-bottom-left-radius: 0.25rem;
+		border-bottom-right-radius: 0.25rem;
+		background-color: var(--background-highlighted);
+		width: 100%;
+		position: relative;
+	}
+`;
+
+interface IImageProps {
+	image?: string;
+}
+export const CustomImage = styled.div<IImageProps>`
+	max-height: 150px;
+	min-height: 200px;
+	width: 100%;
+	border-top-left-radius: 0.25rem;
+	border-top-right-radius: 0.25rem;
+	background-size: cover;
+	background-repeat: no-repeat;
+
+	-webkit-box-shadow: 8px 8px 11px -4px rgba(0, 0, 0, 0.75);
+		-moz-box-shadow: 8px 8px 11px -4px rgba(0, 0, 0, 0.75);
+		box-shadow: 8px 18px 11px -4px rgba(0, 0, 0, 0.75);
+
+	${(props) =>
+		props.image &&
+		css`
+			background-position: ${props.image ===
+			'/static/media/dragon1.38277ad6.jpg' ? 'bottom': 'inherit'};
+			background-image: url(${props.image});
+		`}
+`;
+
+export const Buttons = styled.div`
+	display: flex;
+	width: 100%;
+	margin-top: 30px !important;
+
+	button + button {
+		margin-left: 1rem;
+	}
 `;
 
 export default Container;
