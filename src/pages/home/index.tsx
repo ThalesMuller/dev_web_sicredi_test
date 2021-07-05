1810a6d1fa62a2f5c7ce81f89c985c76e4bb7877
ThalesMuller <thales.em@gmail.com>
2021-07-04 22:54:30 -0300

    [ADD] squeleton loading for dragon list and small layout fixes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,10 +5,11 @@ import Container, {
 	ButtonRound,
 	BasicInfo,
 	Info,
+	IdContainer,
 } from './styles';
 import { deleteDragon, getAllDragons } from '../../services/api';
 import IDragon from '../../interfaces/IDragon';
-import { Button, DisplayId, CreatedAt } from '../../components';
+import { Button, CreatedAt, DisplayId } from '../../components';
 import { useHistory } from 'react-router-dom';
 import {
 	FiAlignCenter,
@@ -17,9 +18,202 @@ import {
 	FiUser,
 } from 'react-icons/fi';
 
+const initialDragonLIst: IDragon[] = [
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+	{
+		createdAt: '2021-06-25T21:07:29.475Z',
+		name: '.....',
+		type: '.....',
+		histories: '...................................',
+		id: '-',
+	},
+];
+
 export default function Details(): JSX.Element {
 	const history = useHistory();
-	const [dragonList, setDragonList] = useState<IDragon[]>([]);
+	const [dragonList, setDragonList] = useState<IDragon[]>(initialDragonLIst);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		try {
@@ -40,10 +234,12 @@ export default function Details(): JSX.Element {
 	};
 
 	const loadDragons = async () => {
+		setLoading(true);
 		const data = await getAllDragons();
-		const sortedList = data.data.sort(sortById);
+		const sortedList = data?.data.sort(sortById);
 
-		setDragonList(sortedList);
+		setDragonList(sortedList ? sortedList : []);
+		setLoading(false);
 	};
 
 	const handleSelectDragon = (dragon: IDragon) => {
@@ -80,8 +276,11 @@ export default function Details(): JSX.Element {
 		return (
 			<Container key={`dragon-${index}`}>
 				<BasicInfo onClick={() => handleSelectDragon(dragon)}>
-					{dragon.createdAt && <CreatedAt date={dragon.createdAt} />}
-					{dragon.id && <DisplayId>{dragon.id}</DisplayId>}
+					{dragon.id && (
+						<IdContainer>
+							<DisplayId isHome>{dragon.id}</DisplayId>
+						</IdContainer>
+					)}
 
 					{dragon.name && (
 						<Info>
@@ -103,6 +302,7 @@ export default function Details(): JSX.Element {
 							</div>
 						</Info>
 					)}
+					{dragon.createdAt && <CreatedAt date={dragon.createdAt} />}
 				</BasicInfo>
 				<Buttons>
 					<Button onClick={() => handleDeleteDragon(dragon)}>
